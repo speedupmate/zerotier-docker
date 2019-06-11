a6100386818f495fa3d4e7142ba0c422ecb7f513
Anton <anton@speedupmate.com>
2019-06-11 10:34:37 +0300

    zt ver to 1.2.12

--- alpine.sh
+++ alpine.sh
@@ -1,7 +1,7 @@
 apk add --update alpine-sdk linux-headers
 cd build
 rm -rf dist/*
-curl -s https://codeload.github.com/zerotier/ZeroTierOne/zip/1.2.4 -o zerotier-src.zip
+curl -s https://codeload.github.com/zerotier/ZeroTierOne/zip/1.2.12 -o zerotier-src.zip
 unzip -q zerotier-src.zip
 cd ./ZeroTierOne-1.2.4
 make -f make-linux.mk
